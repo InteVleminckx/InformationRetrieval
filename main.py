d3c2edf29e72de281c883b7cdd49db6ede0040ef
DzhemM <72568513+DzhemM@users.noreply.github.com>
2023-11-29 22:35:52 +0100

    Okapi BM25 added

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from src.vector_space_model import VSM
+from src.okapi_BM25 import BM25
 
 if __name__ == '__main__':
-    dataset = "data/video_games.txt"
+    dataset = "data/video_games_small.txt"
 
     vsm = VSM(dataset)
     vsm.read_document()
+    vsm.rank_documents("fighting video game")
 
-    vsm.rank_documents("")
+    bm25 = BM25(dataset)
+    bm25.read_document()
+    bm25.rank_documents("fighting video game")
