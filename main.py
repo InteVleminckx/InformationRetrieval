6d5a3ba8f23b4c3b0c55f07649f8783e7e353506
Inte Vleminckx <InteVleminckx@student.uantwerpen.be>
2023-12-07 15:30:34 +0100

    Apply caching

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from src.data_pre_processing import DataPreProcessing
+from src.bert import BERTSimilarity
+from src.data_pre_processing import get_preprocessed_data, DataPreProcessing
 from src.okapi_BM25 import BM25
 from src.vector_space_model import VSM
 
 if __name__ == '__main__':
     dataset = "data/video_games.txt"
 
-    data_pre_processor = DataPreProcessing()
-    data_pre_processor.read_dataset(dataset)
+    preprocessed_result = get_preprocessed_data(dataset)
 
-    print("VSM")
-    vsm = VSM(data_pre_processor)
-    vsm.rank_documents("fighting video game")
-    print("\nBM25")
-    bm25 = BM25(data_pre_processor)
-    bm25.rank_documents("fighting video game")
+    data_pre_processor = DataPreProcessing(*preprocessed_result)
+
+    # print("VSM")
+    # vsm = VSM(data_pre_processor)
+    # vsm.rank_documents("fighting video game")
+    # print("\nBM25")
+    # bm25 = BM25(data_pre_processor)
+    # bm25.rank_documents("fighting video game")
+    print("\nBert")
+    bert = BERTSimilarity(data_pre_processor)
+    bert.rank_documents("fighting video game")
