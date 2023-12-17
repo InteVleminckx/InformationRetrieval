0a4b2211188be407ef1abf659314bbd2b560bb56
DzhemM <72568513+DzhemM@users.noreply.github.com>
2023-12-17 16:08:37 +0100

    Basic UI

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import json
 
 from src.utils import *
 
@@ -9,10 +10,17 @@ data_preprocessor = get_preprocessed_data("video_games.txt")
 
 
 @app.route('/')
+@app.route('/<documents>')
 def index():
-    global hello
-    print(hello, flush=True)
-    return render_template('index.html')
+    global data_preprocessor
+    titles = data_preprocessor[0].titles
+    #print(titles, flush=True)
+
+    data = {
+        "titles": titles
+    }
+
+    return render_template('index.html', data = data)
 
 
 if __name__ == "__main__":
